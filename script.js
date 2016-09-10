4b4229a9f7f3d556db93a91adf56e6a1addbddc5
ethanhowell <ehowell_2010@live.com>
2016-09-10 11:28:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+const second = 1000;
+const framesPerSecond = 60;
+const interval = second / framesPerSecond;
+
 $(document).ready(function() {
 	var time = 0;
-	var second = 1000;
-	var framesPerSecond = 60;
-	var interval = second / framesPerSecond;
 
-	var earthMotionMultiplier = 1 / 900;
-	var moonMotionMultiplier = 1 / 200;
+	var earthSystemMotionMultiplier = setMultiplier(10);
+	var moonMotionMultiplier = setMultiplier(5 / 6);
 
-	var earth = $("#earth");
-	var moon = $("#earth .moon");
+	var earthSystem = $("#earthSystem");
+	var moon = $("#earthSystem .moon");
 
 	setInterval(function() {
-		ymotion(time += interval, earth, earthMotionMultiplier);
-		xmotion(time, earth, earthMotionMultiplier);
-		ymotion(time, moon, moonMotionMultiplier);
-		xmotion(time, moon, moonMotionMultiplier);
+		ymotion(time += interval, earthSystem, earthSystemMotionMultiplier, 400);
+		xmotion(time, earthSystem, earthSystemMotionMultiplier, 600);
+		ymotion(time, moon, moonMotionMultiplier, 100);
+		xmotion(time, moon, moonMotionMultiplier, 140);
+		// var angle = Math.atan2(earthSystem.parent().height()  / 2 - earthSystem.position().top, earthSystem.parent().width() / 2 - earthSystem.position().left);
+		// var angle1 = Math.atan2(moon.parent().height()  / 2 - moon.position().top, moon.parent().width() / 2 - moon.position().left);
+		// $("#sun").css({transform: "rotateZ(" + angle + "rad)"});
+		// $("#rock").css({transform: "rotateZ(" + angle1 + "rad)"});
 	}, interval);
 });
 
-function ymotion(time, element, multiplier) {
-	element.css({ bottom: ((Math.sin(time * multiplier) + 1) * (element.parent().height() - element.height()) / 2) + "px" });
+function ymotion(time, element, multiplier, distanceToCenter) {
+	element.css({ bottom: (((Math.sin(time * multiplier)) * (distanceToCenter) + (element.parent().height() - element.height()) / 2)) + "px" });
 }
 
-function xmotion(time, element, multiplier) {
-	element.css({ left: ((Math.cos(time * multiplier) + 1) * (element.parent().width() - element.width()) / 2) + "px" });
+function xmotion(time, element, multiplier, distanceToCenter) {
+	element.css({ left: (((Math.cos(time * multiplier)) * (distanceToCenter) + (element.parent().width() - element.width()) / 2)) + "px" });
+}
+
+function setMultiplier(secondsPerRevolution) {
+	return 2 * Math.PI / (second * secondsPerRevolution);
 }
