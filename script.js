dd342eb23d92bad24001b6690c77477ce8942cde
ethanhowell <ehowell_2010@live.com>
2016-09-09 22:03:01 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ $(document).ready(function() {
 	var time2 = 0;
 
 	window.setInterval(function() {
-		ymotion(time += 1000 / 60, $("#earth"));
-		xmotion(time, $("#earth"));
+		ymotion(time += 1000 / 60, $("#earth"), 1/900);
+		xmotion(time, $("#earth"), 1/900);
+	}, 1000 / 60);
+
+	window.setInterval(function() {
+		ymotion(time2 += 1000 / 60, $("#earth .moon"), 1/200);
+		xmotion(time2, $("#earth .moon"), 1/200);
 	}, 1000 / 60);
 });
 
-function ymotion(time, element) {
-	element.css({ bottom: ((Math.sin(time / 800) + 1) * ($(window).height() - element.height()) / 2) + "px" });
+function ymotion(time, element, multiplier) {
+	
+	element.css({ bottom: ((Math.sin(time * multiplier) + 1) * (element.parent().height() - element.height()) / 2) + "px" });
 }
 
-function xmotion(time, element) {
-	element.css({ left: ((Math.cos(time / 800) + 1) * ($(window).width() - element.width()) / 2) + "px" });
+function xmotion(time, element, multiplier) {
+	element.css({ left: ((Math.cos(time * multiplier) + 1) * (element.parent().width() - element.width()) / 2) + "px" });
 }
